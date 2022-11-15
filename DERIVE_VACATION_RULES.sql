21a6340b6569185768ff0a94fb47c8636ea5364a
renusrkv <115226342+renusrkv@users.noreply.github.com>
2022-11-15 13:44:56 +0530

    Update DERIVE_VACATION_RULES.sql

--- DERIVE_VACATION_RULES.sql
+++ DERIVE_VACATION_RULES.sql
@@ -1,63 +1,15 @@
-    FUNCTION DERIVE_VACATION_RULES (p_item_key         VARCHAR2,
-                                    p_orig_approver    VARCHAR2)
-        RETURN VARCHAR2
-    IS
-        l_vac_approver   VARCHAR2 (50);
-    BEGIN
-        l_vac_approver := NULL;
-
-        SELECT ACTION_ARGUMENT
-          INTO l_vac_approver
-          FROM WF_ROUTING_RULES
-         WHERE     role = p_orig_approver
-               AND MESSAGE_TYPE = 'LSUONBDR' -- commented by rvemula as it should work for all item types  30-May-2019  -- Enable the message_type to work vacation rule for Supplier Onbaording alone ITSM-64088
-               AND SYSDATE BETWEEN TRUNC (BEGIN_DATE) AND TRUNC (END_DATE);
-
-        --rvemula ITSM-145252 below insert
-        INSERT INTO LOTC_SUPP_ONBDR_VACATION_RULES (REGI_REQUEST_NUMBER,
-                                                    ITEM_TYPE,
-                                                    ITEM_KEY,
-                                                    CREATED_BY,
-                                                    CREATION_DATE,
-                                                    ACTUAL_USER,
-                                                    DELEGATED_TO,
-                                                    LAST_UPDATED_BY,
-                                                    LAST_UPDATE_DATE,
-                                                    LAST_UPDATE_LOGIN)
-            SELECT SUBSTR (p_item_key, 1, INSTR (p_item_key, '_', 1) - 1),
-                   'LSUONBDR',
-                   p_item_key,
-                   (SELECT USER_ID
-                      FROM fnd_user fu
-                     WHERE Fu.USER_NAME = l_vac_approver AND ROWNUM = 1),
-                   SYSDATE,
-                   p_orig_approver,
-                   l_vac_approver,
-                   (SELECT USER_ID
-                      FROM fnd_user fu
-                     WHERE Fu.USER_NAME = l_vac_approver AND ROWNUM = 1),
-                   SYSDATE,
-                   fnd_global.login_id
-              FROM DUAL
-             WHERE     1 = 1
-                   AND NOT EXISTS
-                           (SELECT 1
-                              FROM LOTC_SUPP_ONBDR_VACATION_RULES RL1
-                             WHERE     1 = 1
-                                   AND ITEM_KEY = p_item_key
-                                   AND ACTUAL_USER = p_orig_approver
-                                   AND DELEGATED_TO = l_vac_approver);
-
-        RETURN l_vac_approver;
-    EXCEPTION
-        WHEN NO_DATA_FOUND
-        THEN
-            RETURN p_orig_approver;
-        WHEN OTHERS
-        THEN
-            --Enable the log for future reference 23-MAY-19
-            WRITE_CUST_LOG (p_item_key,
-                            'DERIVE_VACATION_RULES',
-                            'Error is ' || SQLERRM);
-            RETURN p_orig_approver;
-    END DERIVE_VACATION_RULES;
+select rh.segment1 REQ_NUM, rh.org_id, rl.requisition_line_id,
+rl.requisition_header_id, ph.segment1 RFQ_NUM, ph.org_id,
+pl.po_header_id, pl.po_line_id,
+to_char(pl.creation_date, 'DD-Mon-RRRR HH24:MI:SS') cre_date,
+ph.type_lookup_code, ph.from_type_lookup_code
+from apps.po_requisition_headers_all rh,apps.po_requisition_lines_all rl,
+apps. po_headers_all ph,apps.po_lines_all pl
+where rh.requisition_header_id = rl.requisition_header_id
+  and rl.last_update_date =pl.creation_date
+  and pl.po_header_id = ph.po_header_id
+  and rl.on_rfq_flag = 'Y'
+  AND rh.segment1 = 'xxxxx '
+  and rh.org_id = 'xxxxx'
+-- and ph.segment1 = '&RFQ_NUM'
+order by requisition_line_id desc
